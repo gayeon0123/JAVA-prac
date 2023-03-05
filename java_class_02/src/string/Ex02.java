dc2e3bb27bb445b4fec0bcaebf1875b7f6f473c3
gayeon Kim <95395033+gayoun0123@users.noreply.github.com>
2023-03-06 00:13:30 +0900

    Add files via upload

---
+++ java_class_02/src/string/Ex02.java
@@ -0,0 +1,19 @@
+package string;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// 문자열 : String
+		// String은 기본자료형처럼 다루게 해주는 class
+		
+		String str1 = "\"홍길동\"";
+		
+		System.out.println(str1);
+		
+		String str2 = "이몽룡과\t성춘향은\n사랑하는\\사이다.";
+		
+		System.out.println(str2);
+		
+	}
+
+}
